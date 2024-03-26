34094b9cd03da8c0e2647d1017e04d386975798a
ComTech <shumailagulfam3@gmail.com>
2024-03-26 13:44:08 +0500

    second comit

---
+++ 3.Name Cases/main.js
@@ -0,0 +1,7 @@
+// 3.Name Cases: Store a person’s name in a variable, and then print that person’s name in 
+// lowercase, uppercase, and titlecase.
+let personName = "Asharib";
+console.log(personName.toLowerCase());
+console.log(personName.toUpperCase());
+console.log(personName.charAt(0).toUpperCase() + personName.slice(1).toLowerCase());
+export {};
