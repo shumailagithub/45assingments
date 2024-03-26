7a0ac31e4e95557f52e86c7d62334649aa332815
ComTech <shumailagulfam3@gmail.com>
2024-03-26 12:54:07 +0500

    first commit

---
+++ 2.Personal Message/main.js
@@ -0,0 +1,7 @@
+//2. Personal Message: Store a person’s name in a variable, and print a message to that person. 
+// Your message should be simple, such as, “Hello Eric, would you like to learn some Python today?”
+var personName = "Eric";
+console.log("Hello ".concat(personName, "! would you like to learn some python today? "));
+// let personName : string = "Eric";
+// let message : string ='"would you like to learn some python today? "';
+//console.log(" Hello! " + personName + message);
