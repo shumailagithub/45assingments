2810f8cf2385e991de656a9dbe632ec5d061bf55
ComTech <shumailagulfam3@gmail.com>
2024-03-27 16:26:53 +0500

    1 change

---
+++ 8.Number-Eight/main.ts
@@ -0,0 +1,21 @@
+// 8.You should create four lines that look like this:
+
+// console.log(5 + 3)
+
+//Your output should simply be four lines with the number 8 appearing once on each line.
+
+
+
+
+
+         //Addition
+console.log(5 + 3);       //8
+
+        //Subtraction 
+console.log(10 - 2);      //8
+
+        //multiplication
+console.log(2 * 4);       //8
+
+        //Division
+console.log(16 / 2);      //8
