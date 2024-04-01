ef26ba0f3f1b531287b8772e02b7c7618e210bce
ComTech <shumailagulfam3@gmail.com>
2024-04-01 16:40:00 +0500

    first commit

---
+++ 11.Names/main.js
@@ -0,0 +1,5 @@
+let frendsNames = ["Asma", "Ayesh", "Anila", "Aqsa"];
+for (let i = 0; i < frendsNames.length; i++) {
+    console.log(frendsNames[i]);
+}
+export {};
---
+++ 11.Names/main.ts
@@ -0,0 +1,12 @@
+// 11.Names: Store the names of a few of your friends in a array called names. 
+// Print each person’s name by accessing each element in the list, one at a time.
+
+
+
+let frendsNames :string [] = ["Asma", "Ayesh", "Anila", "Aqsa"];
+
+for (let i = 0; i < frendsNames.length; i++) {
+    console.log(frendsNames[i]);
+    
+    
+}
