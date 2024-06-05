ee62ee55998e27ac14bccb30c979b3ae6c3070b0
ComTech <shumailagulfam3@gmail.com>
2024-06-05 18:51:58 +0500

    first commit

---
+++ 27.Alien_Colors_3/index.ts
@@ -0,0 +1,44 @@
+// Q No 27:
+// Alien Colors #3: Turn your if-else chain from Exercise 5-4 into an if-else chain.
+// • If the alien is green, print a message that the player earned 5 points.
+
+// • If the alien is yellow, print a message that the player earned 10 points.
+
+// • If the alien is red, print a message that the player earned 15 points.
+
+// • Write three versions of this program, making sure each message is printed for the appropriate color alien.
+//------------------------------------------------------------------------------------------------------------------
+
+// • If the alien is green, print a message that the player earned 5 points.
+//Version 1
+let Alien_Color_1 : string = "green";
+
+if (Alien_Color_1 == "green") {
+    console.log("the player earned 5 points.");
+}else {
+    console.log("the player earned 15 points.");
+    
+}
+
+//------------------------------------------------------------------------------
+// • If the alien is yellow, print a message that the player earned 10 points.
+// version 2
+let Alien_Color_2 :string = "yellow";
+
+if (Alien_Color_2 == "yellow") {
+    console.log("the player earned 10 points.");
+}else {
+    console.log("the player earned 15 points.");
+    
+}
+//--------------------------------------------------------------------------------
+// • If the alien is red, print a message that the player earned 15 points.
+//Version 3
+let Alien_Color_3 :string = "red";
+
+if (Alien_Color_3 == "red") {
+    console.log("the player earned 15 points");
+}else{
+    console.log("the player earned 5 points.");
+    
+}
---
+++ 29.Favorite_Fruit/index.ts
@@ -0,0 +1,29 @@
+// Q No 29:
+// Favorite Fruit: Make a array of your favorite fruits, and then write a series of independent if statements that check for certain fruits in your array.
+// • Make a array of your three favorite fruits and call it favorite_fruits.
+
+// • Write five if statements. Each should check whether a certain kind of fruit is in your array. 
+//If the fruit is in your array, the if block should print a statement, such as You really like bananas!
+
+let favorite_fruits:string[] = ["Kiwi", "Grapes","bananas"];
+
+if (favorite_fruits.includes("bananas")) {
+    console.log("I rally like bananase!");
+    
+}
+if (favorite_fruits.includes("Kiwi")) {
+    console.log("I really like Kiwi!");
+    
+}
+if (favorite_fruits.includes("Mango")) {
+    console.log("I really like Mango!");
+    
+}
+if (favorite_fruits.includes("Grapes")) {
+    console.log("I really like Grapes!");
+    
+}
+if (favorite_fruits.includes("PineApple")) {
+    console.log("I really like PineApple!");
+    
+}
